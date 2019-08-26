9f33018aea1a7e6f6a264dcf2de067f3391c0ff2
Igor Stureiko <stureiko@mail.ru>
2019-08-26 22:02:54 +0300

    Добавлен if и цикл while

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 print('Hello World!')
 
 ale = 71
-#age = int(input('Сколько вам лет: '))
-age = 35
+age = int(input('Сколько вам лет: '))
 
 after20 = age + 20
 print('Через 20 лет вам будет:', after20)
 
 alive = ale - age
-print('Вам осталось жить:', alive)
+print('Вам осталось жить:', alive, 'лет.')
 
 count = age*365*24*60
-print('Вы прожили', count, 'секунд.')
+print('Вы прожили', count, 'секунд.\n')
+
+if age < 18:
+    print('Доступ запрещен!')
+else:
+    print('Доступ разрешен!')
