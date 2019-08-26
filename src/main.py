358c38bdbd6c87fa5421129d7c96fd2ff78c070d
Igor Stureiko <stureiko@mail.ru>
2019-08-26 21:27:01 +0300

    add basic math operations

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+print('Hello World!')
+
+ale = 71
+#age = int(input('How old are you: '))
+age = 35
+
+after20 = age + 20
+print "After 20 years you will be:", after20
+
+alive = ale - age
+print 'You have to live:', alive
+
+count = age*365*24*60
+print 'You lived', count, 'seconds.'
