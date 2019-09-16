550a84d35767d5afecbae2206f880d0eb0d229f5
Igor Stureiko <stureiko@mail.ru>
2019-09-16 10:44:40 +0300

    Lesson6 - работа с тернарным оператором перевод четных букв в слове в upper

--- src/Lesson7.py
+++ src/Lesson7.py
@@ -7,3 +7,18 @@ print('Max' if condition else 'Min')
 print('Возвращаем ложь')
 condition = False
 print('Max' if condition else 'Min')
+
+# слово -> СлОвО
+word = 'слово'
+result = []
+
+for i in range(len(word)):
+    letter = word[i]
+    letter = letter.lower() if i % 2 != 0 else letter.upper()
+    result.append(letter)
+
+result = ''.join(result)
+print(result)
+
+# ввод пароля
+
