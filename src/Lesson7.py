ad591497339b384929bff69a8d13261b19ed3d24
Igor Stureiko <stureiko@mail.ru>
2019-09-16 10:26:28 +0300

    Lesson6 - работа с тернарным оператором.

---
+++ src/Lesson7.py
@@ -0,0 +1,9 @@
+# Тернарный оператор
+# результат 1 если условие иначе результат 2
+print('Возвращаем истину')
+condition = True
+print('Max' if condition else 'Min')
+
+print('Возвращаем ложь')
+condition = False
+print('Max' if condition else 'Min')
