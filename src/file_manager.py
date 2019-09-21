272f571303befdcf02f9330657f301216d706434
Igor Stureiko <stureiko@mail.ru>
2019-09-21 09:42:19 +0300

    Lesson8 - добавлена возможность работать в интерактивном режиме. Добавлена возможность вызова игры "Угадай число"

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import datetime
+import random
 
 
 # функция создания файла
@@ -17,11 +18,24 @@ def create_folder(name):
         print('Такая папка уже есть')
 
 
-def get_list(folders_only=False):
-    result = os.listdir()
+def get_list(path, folders_only=False):
+    if path:
+        try:
+            result = os.listdir(path)
+        except FileNotFoundError:
+            print('Такого пути не найдено')
+    else:
+        result = os.listdir()
     if folders_only:
         result = [f for f in result if os.path.isdir(f)]
     print(result)
+
+
+# def get_list(folders_only=False):
+#     result = os.listdir()
+#     if folders_only:
+#         result = [f for f in result if os.path.isdir(f)]
+#     print(result)
 
 
 def delete_f(name):
@@ -49,8 +63,50 @@ def save_info(message):
         f.write(result + '\n')
 
 
+def play():
+    number = random.randint(1, 100)
+    user_number = None
+    count = 0
+    # Выбераем уровень сложности
+    # делаем проверку на валидность ввода
+    levels = {1: 10, 2: 5, 3: 3}
+    while True:
+        level = input('Введите уровень сложности от 1 до 3: ')
+        if level.isdigit():
+            if int(level) in levels.keys():
+                print(f'Вы выбрали {int(level)} уровень')
+                break
+            else:
+                print(f'Такого уровня не предусмотрено.')
+                continue
+        else:
+            print('Вы ввели не число')
+            continue
+
+    max_count = levels[int(level)]
+
+    while number != user_number:
+        count += 1
+        if count > max_count:
+            print('Просрали !!!')
+            break
+        print(f'Попытка № {count}')
+        user_number = input('Введите число от 1 до 100: ')
+        if user_number.isdigit():
+            user_number = int(user_number)
+        else:
+            print('Вы ввели не число. Попробуйте еще раз.')
+            continue
+        if number > user_number:
+            print('Ваше число меньше загаданного.')
+        elif number < user_number:
+            print('Ваше число больше загаданного')
+    else:
+        print('Победа!')
+
 if __name__ == '__main__':
     current_path = os.getcwd()
     get_list(True)
     delete_f('test')
     save_info('Test message')
+
--- src/main.py
+++ src/main.py
@@ -2,55 +2,91 @@
 # с параметрами (на уроке разбирали на примере одной функции).
 
 import sys
-from file_manager import create_file, create_folder, get_list, delete_f, copy_f, save_info
+import os
+from file_manager import create_file, create_folder, get_list, delete_f, copy_f, save_info, play
+
+
+def main_work(arg):
+    try:
+        command = arg[0]
+    except IndexError:
+        command = 'help'
+
+    save_info(f'Аргумент: {command}')
+
+    if command == 'list':
+        try:
+            path = arg[1]
+        except IndexError:
+            path = ''
+        get_list(path)
+    elif command == 'create_file':
+        try:  # проверяем наличие имени файла
+            name = arg[1]
+        except IndexError:
+            print('Отсутствует имя файла')
+        else:
+            try:
+                text = arg[2]  # проверяем наличие текста для записи в файл
+                create_file(name, text)  # создаем файл с текстом
+            except IndexError:  # если текста нет
+                create_file(name)  # тогда просто создаем пустой файл
+    elif command == 'create_folder':
+        try:
+            name = arg[1]  # если есть имя папки
+        except IndexError:
+            print('Отсутствует имя файла')
+        else:
+            create_folder(name)  # создаем папку с переданным именем
+    elif command == 'delete':
+        try:
+            name = arg[1]  # если передано имя папки или файла
+        except IndexError:
+            print('Отсутствует имя файла')
+        else:
+            delete_f(name)  # удаляем указанную папку
+    elif command == 'copy':
+        try:  # для копирования должны присутствоват как имя копируемого объекта
+            name = arg[1]  # так и имя нового объекта
+            new_name = arg[2]
+        except IndexError:
+            print('Отсутствует имя файла')
+        else:
+            copy_f(name, new_name)
+    elif command == 'play':
+        play()
+    else:
+        print('i - переход в интерактивный режим')
+        print('list - список файлов и папок')
+        print('create_file - создание файла')
+        print('create_folder - создание папки')
+        print('delete - удаление файла или папки')
+        print('copy - копирование файла или папки')
+        print('play - игра \"Угадай число\"')
+        print('help - список команд')
+    save_info('завершение')
+
 
 save_info('Запуск')
 
-try:
-    command = sys.argv[1]
-except IndexError:
-    command = 'help'
+# print(sys.argv)
 
-save_info(f'Аргумент: {command}')
-
-if command == 'list':
-    get_list()
-elif command == 'create_file':
-    try:  # проверяем наличие имени файла
-        name = sys.argv[2]
-    except IndexError:
-        print('Отсутствует имя файла')
-    else:
-        try:
-            text = sys.argv[3]  # проверяем наличие текста для записи в файл
-            create_file(name, text)  # создаем файл с текстом
-        except IndexError:  # если текста нет
-            create_file(name)  # тогда просто создаем пустой файл
-elif command == 'create_folder':
-    try:
-        name = sys.argv[2]  # если есть имя папки
-    except IndexError:
-        print('Отсутствует имя файла')
-    else:
-        create_folder(name)  # создаем папку с переданным именем
-elif command == 'delete':
-    try:
-        name = sys.argv[2]  # если передано имя папки или файла
-    except IndexError:
-        print('Отсутствует имя файла')
-    else:
-        delete_f(name)  # удаляем указанную папку
-elif command == 'copy':
-    try:  # для копирования должны присутствоват как имя копируемого объекта
-        name = sys.argv[2]  # так и имя новогообъекта
-        new_name = sys.argv[3]
-    except IndexError:
-        print('Отсутствует имя файла')
-    else:
-        copy_f(name, new_name)
+if len(sys.argv) < 2:
+    arguments = 'help'
 else:
-    print('list - список файлов и папок')
-    print('create_file - создание файла')
-    print('create_folder - создание папки')
-    print('delete - удаление файла или папки')
-    print('copy - копирование файла или папки')
+    arguments = sys.argv[1:]
+
+if arguments[0] == 'i':
+    print('Интерактивный режим')
+    while True:
+        arguments = input('Введите команду: ').split()
+        if len(arguments) == 0:
+            arguments.append('help')
+        else:
+            if arguments[0] == 'exit':
+                break
+        # print(arguments)
+        main_work(arguments)
+else:
+    main_work(arguments)
+
