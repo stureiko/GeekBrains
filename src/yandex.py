cf576c4a2456fec444bfdc7d851e218108e327d9
Igor Stureiko <stureiko@mail.ru>
2019-09-27 20:01:15 +0300

    Парсем Yandex в он-лайне - получаем с него погоду и пробки

--- src/yandex.py
+++ src/yandex.py
@@ -21,3 +21,10 @@ li_temp = [s.get_text() for s in li if s.get('class') == ['weather__forecast']]
 li_temp = ' '.join(li_temp)
 print(li_temp)
 
+li3 = [s.get_text() for s in li if s.get('class') == ['traffic__rate-text']]
+traffic = ' '.join(li3)
+
+traf_for = [s.get_text() for s in li if s.get('class') == ['traffic__forecast']]
+traf_for = ' '.join(traf_for)
+
+print(traffic + ' баллов - ' + traf_for)
