4e45f2ce79094347db29c2b437e1d102383b2413
Igor Stureiko <stureiko@mail.ru>
2019-10-01 17:19:27 +0400

    Получаем страницу из  wikipedia

---
+++ src/wikipedia.py
@@ -0,0 +1,20 @@
+# https://ru.wikipedia.org/wiki/Дерево
+from requests import get
+
+
+def get_link(topic: str):
+    link = "https://ru.wikipedia.org/wiki/" + topic.capitalize()
+    return link
+
+
+def get_topic_page(topic: str):
+    link = get_link(topic)
+    html_content = get(link).text
+    with open(topic+".html", "w", encoding="utf-8") as f:
+        f.write(html_content)
+    return True
+
+
+if __name__ == '__main__':
+    get_topic_page("дерево")
+
