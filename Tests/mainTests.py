5e6a88129ea4e85261c5cea137d05680997f74d0
Igor Stureiko <stureiko@mail.ru>
2019-10-08 17:37:23 +0300

    Добавили класс игрока и оттестировали его поведение

--- Tests/mainTests.py
+++ Tests/mainTests.py
@@ -1,13 +1,30 @@
 import unittest
-import src.avasales as aviasales
+from src.player import Player
+from src.game import Game
 
 
-class aviasalesTest(unittest.TestCase):
-    def test_blank(self):
-        self.assertEqual(aviasales.iata_search(''), [])
-    #
-    # def test_city(self):
-    #     self.assertEqual(aviasales.iata_search('Москва'), ['Russia', 'Moscow', 'MOW'])
+class PlayerTest(unittest.TestCase):
+    def test_default_health(self):
+        p = Player()
+        self.assertEqual(100, p.health, 'Здоровье игрока по умолчанию должно быть равно 100')
+
+    def test_default_damage(self):
+        p = Player()
+        self.assertEqual(10, p.default_damage, 'Удар игрока по умолчанию должен быть равен 10')
+
+    def test_default_position(self):
+        p = Player()
+        self.assertListEqual([0, 0], p.position, 'Координаты игрока по умолчанию равны [0, 0]')
+
+
+class GameTest(unittest.TestCase):
+    def test_default_game_player_health(self):
+        game = Game()
+        self.assertEqual(100, game.player.health, 'Здоровье игрока созданного по умолчанию должно быть равно 100')
+
+    def test_default_game_player_damage(self):
+        game = Game()
+        self.assertEqual(10, game.player.default_damage, 'Удар игрока созданного по умолчанию должен быть равен 10')
 
 
 if __name__ == '__main__':
---
+++ src/player.py
@@ -0,0 +1,8 @@
+class Player:
+    """
+    Класс игрока
+    """
+    health = 100
+    default_damage = 10
+    position = [0, 0]
+
