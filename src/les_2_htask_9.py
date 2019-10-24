ea56ab93f381704268b94d2260a0446b70bafead
Igor Stureiko <stureiko@mail.ru>
2019-10-24 21:11:50 +0500

    Получить от пользователя числа, найти среди них с максимальной суммой цифр. После окончания ввода - вывести это число и сумму его цифр

---
+++ src/les_2_htask_9.py
@@ -0,0 +1,27 @@
+"""
+9. Среди натуральных чисел, которые были введены, найти наибольшее по сумме цифр.
+Вывести на экран это число и сумму его цифр.
+"""
+
+
+def sum_num(n: int) -> int:
+    res: int = 0
+    while n > 0:
+        res += n % 10
+        n //= 10
+    return res
+
+
+res = 0
+res_num = 0
+
+while True:
+    num = input('Введите число, для выхода введите любой символ кроме числа: ')
+    if num.isdigit():
+        num = int(num)
+        if res < sum_num(num):
+            res = sum_num(num)
+            res_num = num
+    else:
+        print(f'Число = {res_num}, сумма его цифр = {res}')
+        break
