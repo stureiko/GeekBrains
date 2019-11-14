479b480231ea4cb8b5e82ded2b5688b65f48a9bb
Igor Stureiko <stureiko@mail.ru>
2019-11-14 21:00:52 +0300

    Сортировка вставкой

---
+++ src/Sort_insert.py
@@ -0,0 +1,20 @@
+import random
+
+size = 10
+array = [i for i in range(size)]
+random.shuffle(array)
+print(array)
+
+def insertion_sort(array: list):
+    for i in range(1, len(array)):
+        spam = array[i]
+        j = i
+
+        while array[j - 1] > spam and j > 0:
+            array[j] = array[j - 1]
+            j -= 1
+        array[j] = spam
+
+insertion_sort(array)
+print(array)
+
