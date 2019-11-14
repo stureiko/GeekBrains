7ed570e87172021239683d16e6931a49a67a84ca
Igor Stureiko <stureiko@mail.ru>
2019-11-14 21:30:49 +0300

    Сортировка Шелла

--- src/Sort_insert.py
+++ src/Sort_insert.py
@@ -5,6 +5,7 @@ array = [i for i in range(size)]
 random.shuffle(array)
 print(array)
 
+
 def insertion_sort(array: list):
     for i in range(1, len(array)):
         spam = array[i]
@@ -14,6 +15,7 @@ def insertion_sort(array: list):
             array[j] = array[j - 1]
             j -= 1
         array[j] = spam
+
 
 insertion_sort(array)
 print(array)
