95df914274547125370c8c43ebd4e37e8fc6af76
Igor Stureiko <stureiko@mail.ru>
2019-11-21 21:28:30 +0300

    Поиск в ширину на неориентированном не взвешенном графе

---
+++ src/wide_find.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+g = [
+    [0, 1, 1, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 1, 0],
+]
+
+def bfs(graph, start, finish):
+    parent = [None for _ in range(len(graph))]
+    is_visited = [False for _ in range(len(graph))]
+
+    deq = deque([start])
+    is_visited[start] = True
+
+    while len(deq) > 0:
+        current = deq.pop()
+
+        if current == finish:
+            # return parent
+            break
+
+        for i, vertex in enumerate(graph[current]):
+            if vertex == 1 and not is_visited[i]:
+                is_visited[i] = True
+                parent[i] = current
+                deq.append(i)
+
+    else:
+        print(f'Из вершины {start} нельзя попасть в вершину {finish}')
+
+    cost = 0
+    way = deque([finish])
+    i = finish
+
+    while parent[i] != start:
+        cost += 1
+        way.appendleft(parent[i])
+        i = parent[i]
+
+    cost += 1
+    way.appendleft(start)
+
+    return f'Путь: из {start} в {finish}: {list(way)}, стоимость в {cost} единиц.'
+
+
+s = 2
+f = 5
+print(bfs(g, s, f))
+
+print(bfs(g, 1, 3))
