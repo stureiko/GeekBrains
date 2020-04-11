5edc95b3d918ae761fa564f3d37090b9ff1fa322
Igor Stureiko <stureiko@mail.ru>
2020-04-11 15:09:19 +0300

    Сбор с Kinopoisk премьер с выделением названия фильма и ссылки на его страницу Причесал формат выдачи

--- WebDataParsing/Lesson_2_BeutifulSoup/src/Lesson_2_Kinopoisk.py
+++ WebDataParsing/Lesson_2_BeutifulSoup/src/Lesson_2_Kinopoisk.py
@@ -2,7 +2,7 @@
 # Получить список премьер
 import requests
 from bs4 import BeautifulSoup as bs
-
+from pprint import pprint
 
 if __name__ == '__main__':
     main_link = 'https://www.kinopoisk.ru'
@@ -13,14 +13,14 @@ if __name__ == '__main__':
     # разбираем страницу
     soup = bs(response, 'lxml')
 
-    # ищем div с классом prem_list - сам список премьер
+    # ищем div с классом "prem_list" - сам список премьер
     films_block = soup.find_all('div', {'class': 'prem_list'})[1]
 
     # извлекаем содержимое - div с фильмами
     films_list = films_block.find_all('div', {'class': 'premier_item'})
 
     # смотрим длину списка
-    print(len(films_list))
+    print(f'Всего {len(films_list)} фильмов.')
 
     # список словарей для хранения информации о фильмах
     films = []
@@ -30,12 +30,17 @@ if __name__ == '__main__':
         # извлекаем из карточки фильма ссылку на его страницу и название
         name = film.find('div', {'class': 'text'}).find('span', {'class': 'name'}).find('a')
 
+        # выделяем жанр
+        genre = film.find('div', {'class': 'text'}).find('div', {'class': 'textBlock'}).find_all('span')[-2]. \
+            getText().replace('(', '').replace(')', '').replace('...', '')
+
         # выделяем ссылку на страницу фильма
         link = name['href']
 
         # выделяем название фильма изаносим данные в словарь
         film_data['name'] = name.getText()
         film_data['link'] = main_link + name['href']
+        film_data['genre'] = genre
 
         # добавляем данные в список с фильмами
         films.append(film_data)
