5eaa20257b4a0ab4650d48ca8df76135a3cdc32f
Igor Stureiko <stureiko@mail.ru>
2020-05-02 12:58:34 +0300

    Зашли в ящик, открыли первое письмо и собрали с него всю информацию.

--- WebDataParsing/Lesson_7_Selenium/src/Lesson_7_hTask_mail.py
+++ WebDataParsing/Lesson_7_Selenium/src/Lesson_7_hTask_mail.py
@@ -16,35 +16,28 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ES
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
-def get_mails():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-notifications")
-    options.add_argument("–disable-infobars")
-    options.add_argument("–enable-automation")
-    options.add_argument("–start-maximized")
-    driver = webdriver.Chrome(
-        '/Users/igor/Documents/Programming/Geekbrains/WebDataParsing/Lesson_7_Selenium/src/chromedriver',
-        options=options)
-
-    driver.get("https://mail.ru")  # заходим на страницу
-
-    elem = driver.find_element_by_id('mailbox:login')
-    elem.send_keys('study.ai_172@mail.ru')
-    elem.send_keys(Keys.ENTER)
-
-    elem = driver.find_element_by_id('mailbox:password')
-    elem.send_keys('NewPassword172')
-    elem.send_keys(Keys.ENTER)
+import time
 
 
+def get_mail_info(driver: webdriver.Chrome):
+    wait = WebDriverWait(driver, 10)
+    title = wait.until(ES.presence_of_element_located((By.XPATH,
+                                                       "//h2[@class='thread__subject thread__subject_pony-mode']")))
+    print(f'Title: {title.text}')
+    letter_from = wait.until(ES.presence_of_element_located((By.XPATH,
+                                                             "//div[@class='letter__author']/span")))
+    print(f'From: {letter_from.text}')
+    letter_date = wait.until(ES.presence_of_element_located((By.XPATH,
+                                                             "//div[@class='letter__author']/div[@class='letter__date']")))
+    print(f'Date: {letter_date.text}')
+    letter_body = wait.until(ES.presence_of_element_located((By.XPATH,
+                                                             "//table[4]//tbody[1]//tr[2]//td[1]//table[1]//tbody[1]")))
+    print(f'Body: {letter_body.text}')
 
-
-
+    return 'OK'
 
 
 if __name__ == '__main__':
-    # get_mails()
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-notifications")
     options.add_argument("–disable-infobars")
@@ -54,18 +47,38 @@ if __name__ == '__main__':
         '/Users/igor/Documents/Programming/Geekbrains/WebDataParsing/Lesson_7_Selenium/src/chromedriver',
         options=options)
 
-    driver.get("https://mail.ru")  # заходим на страницу
+    try:
+        driver.get("https://mail.ru")  # заходим на страницу
 
-    elem = driver.find_element_by_id('mailbox:login')
-    elem.send_keys('study.ai_172@mail.ru')
-    elem.send_keys(Keys.ENTER)
+        elem = driver.find_element_by_id('mailbox:login')
+        elem.send_keys('study.ai_172@mail.ru')
+        elem.send_keys(Keys.ENTER)
 
-    elem = driver.find_element_by_id('mailbox:password')
-    elem.send_keys('NewPassword172')
-    elem.send_keys(Keys.ENTER)
+        elem = driver.find_element_by_id('mailbox:password')
+        elem.send_keys('NewPassword172')
+        elem.send_keys(Keys.ENTER)
 
-    wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 10)
 
-    mail = wait.until(ES.element_to_be_clickable((By.XPATH, "//a[@class='llc js-tooltip-direction_letter-bottom js-letter-list-item llc_pony-mode llc_normal']")))
+        mail = wait.until(ES.element_to_be_clickable((By.XPATH, "//a[@class='llc js-tooltip-direction_letter-bottom js-letter-list-item llc_pony-mode llc_normal']")))
+        mail.click()
 
-    mail.click()
+        get_mail_info(driver)
+
+        # wait = WebDriverWait(driver, 10)
+        # title = wait.until(ES.presence_of_element_located((By.XPATH,
+        #                                              "//h2[@class='thread__subject thread__subject_pony-mode']")))
+        # print(f'Title: {title.text}')
+        # letter_from = wait.until(ES.presence_of_element_located((By.XPATH,
+        #                                              "//div[@class='letter__author']/span")))
+        # print(f'From: {letter_from.text}')
+        # letter_date = wait.until(ES.presence_of_element_located((By.XPATH,
+        #                                             "//div[@class='letter__author']/div[@class='letter__date']")))
+        # print(f'Date: {letter_date.text}')
+        # letter_body = wait.until(ES.presence_of_element_located((By.XPATH,
+        #                                                 "//table[4]//tbody[1]//tr[2]//td[1]//table[1]//tbody[1]")))
+        # print(f'Body: {letter_body.text}')
+
+    finally:
+        driver.quit()
+
