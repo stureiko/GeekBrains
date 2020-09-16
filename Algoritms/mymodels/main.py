d931e6a1e030af5edb538d76a34c6d130b34f02d
Igor Stureiko <stureiko@mail.ru>
2020-09-16 14:43:41 +0300

    Обновлены примеры использования в соотвествии с обновленными моделями

--- Algoritms/mymodels/main.py
+++ Algoritms/mymodels/main.py
@@ -7,6 +7,7 @@ from RandomForestDesign import RandomForestDesign
 from RegressionTree import RegressionTree, R2
 from RandomForestRegression import RandomForestRegression
 from LogRegression import LogRegression, f1_score
+from Boosting import Boosting
 
 
 def class_tree():
@@ -62,7 +63,11 @@ def rand_forest():
 
     n_trees = 10
 
-    my_forest_1 = RandomForestDesign(n_trees=n_trees)
+    my_forest_1 = RandomForestDesign(n_trees=n_trees,
+                                     max_depth=6,
+                                     num_leaves=64,
+                                     min_samples_leaf=5,
+                                     criteria='gini')
     my_forest_1.fit(train_data, train_labels)
     train_answers = my_forest_1.tree_vote(train_data)
     test_answers = my_forest_1.tree_vote(test_data)
@@ -99,6 +104,7 @@ def reg_forest():
     print(f' R2 train: {r2_train:.4f}')
     print(f' R2 test: {r2_test:.4f}')
 
+
 def log_los_regression():
     loglos = LogRegression(iterations=1000, alpha=1e-4, tol=1e-5, bound=1e-5)
 
@@ -117,9 +123,49 @@ def log_los_regression():
     print(f'f1 score: {f1_score(y_true=y, y_pred=y_pred)}')
 
 
+def grad_boost():
+    classification_data, classification_labels = datasets.make_classification(n_samples=1000,
+                                                                              n_features=2, n_informative=2,
+                                                                              n_classes=2, n_redundant=0,
+                                                                              n_clusters_per_class=1, random_state=23)
+    train_data, test_data, train_labels, test_labels = model_selection.train_test_split(classification_data,
+                                                                                        classification_labels,
+                                                                                        test_size=0.3,
+                                                                                        random_state=1)
+
+    n_trees = 10
+    max_depth = 60
+    num_leaves = 5
+    min_samples_leaf = 5
+    criteria = 'gini'
+
+    model_b = Boosting(n_trees=n_trees,
+                       max_depth=max_depth,
+                       num_leaves=num_leaves,
+                       min_samples_leaf=min_samples_leaf,
+                       criteria=criteria)
+    model_b.fit(train_data, train_labels)
+    train_answers = model_b.predict(train_data)
+    test_answers = model_b.predict(test_data)
+
+    # Точность на обучающей выборке
+    train_accuracy = accuracy_metric(train_labels, train_answers)
+    print('\n\nГрадиентный бустинг\n')
+    print(f'n_trees={n_trees}, max_depth={max_depth}, \
+            num_leaves={num_leaves}, \
+            min_samples_leaf={min_samples_leaf}, \
+            criteria={criteria}')
+    print(f' Точность на обучающей выборке: {train_accuracy:.3f}')
+
+    # Точность на тестовой выборке
+    test_accuracy = accuracy_metric(test_labels, test_answers)
+    print(f' Точность на тестовой выборке: {test_accuracy:.3f}')
+
+
 if __name__ == '__main__':
     # class_tree()
     # reg_tree()
-    rand_forest()
+    # rand_forest()
     # reg_forest()
     # log_los_regression()
+    grad_boost()
